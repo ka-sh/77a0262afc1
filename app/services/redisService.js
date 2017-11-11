81fec2180cc72ab953667c683b1cbc4b30316f6e
kareem <kareem.elshahawe@gmail.com>
2017-11-12 00:34:05 +0800

    Refactor redis service
     - This refactor is for enahncing code quality,
       by following promises best practices. No nested promises.

--- app/services/redisService.js
+++ app/services/redisService.js
@@ -1,34 +1,50 @@
 const Promise = require('bluebird');
 const uuid = require('uuid/v4');
+const logger = require('winston');
+
 //TODO:Refactor module to be injected with client
 //TODO:export redis host, port to environment variable
 
 //TODO:Redesign this module to handle race condition
 // in case of multiple instances
 module.exports = function(redisClient) {
-
+    /**
+     * Create token key after making sure it doesn't exist in redis.
+     *@param resolve {function} called when key is created successfull.
+     *@param reject {function} called when there is an issue during key creation.
+     *
+     *NOTE:in case of multiple instances there is a possiblity that
+     *      two instances would come up with the same key, hence we need
+     *      to check first if key exist or not, then we return the key
+     *      only after we persist it.
+     */
     function createKey(resolve, reject) {
         let key = uuid();
+        logger.info(`Checking key  ${key}  for collision`);
         redisClient.getAsync(key)
-            .then(function(data) {
-                if (data == null) {
-                    /**
-                     * It is safer to persist the key before sending it to the user making sure that
-                     * key is 100% taken.
-                     */
-                    redisClient.setAsync(key, JSON.stringify({
-                            status: "in progress"
-                        }))
-                        .then(function(result) {
-                            resolve(key);
-                        });
-                    //TODO:Handle error while persisting keys
-
+            .then(function(value) {
+                /**
+                 * Key not found
+                 */
+                if (value == null) {
+                    logger.info(`Key  ${key} is available....persisting`);
+                    return redisClient.setAsync(key, JSON.stringify({
+                        status: "in progress"
+                    }));
                 } else {
+                    //If key is found try again.
+                    //TODO:Refactor to prevent stack overflow.
+                    logger.warn(`Collision detected for key  ${key}  ..retrying`);
                     createKey(resolve, reject);
                 }
-            }, function(reason) {
-                reject(reason);
+            })
+            .then(function(result) {
+                logger.info(`Key generated successfully : ${key}`);
+                resolve(key);
+            })
+            .catch(function(error) {
+                logger.error(`Failed to generate token: ${error}`);
+                reject(error);
             });
     }
 
@@ -44,11 +60,10 @@ module.exports = function(redisClient) {
         getToken: function(token) {
             return new Promise(function(resolve, reject) {
                 redisClient.getAsync(token)
-                    .then(function(result) {
-                        resolve(JSON.parse(result));
-                    }, function(error) {
-                        reject(error);
-                    });
+                    .then(function(value) {
+                        resolve(JSON.parse(value));
+                    })
+                    .catch(reject);
             });
         },
         updateToken: function(token, update) {
